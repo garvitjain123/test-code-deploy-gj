df319eaf3731f403ba263f6483b44276c21a3e3a
Garvit Jain <garvit.jain@highskyit.com>
2023-08-08 14:30:59 +0530

    main - updated port number and app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Sample Angular App</h1>
+    <ul>
+      <li *ngFor="let item of items">{{ item }}</li>
+    </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample-angular-app';
+  items: string[] = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
 }
